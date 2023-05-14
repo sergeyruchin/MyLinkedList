d49af7a530e0575368d469452264d92321a73a00
Sergey <casper_net@mail.ru>
2023-05-14 16:54:30 +0300

    create my class LinkedList and Node

---
+++ MyLinkedList.java
@@ -0,0 +1,30 @@
+public class MyLinkedList {
+    private Node head;
+    private int size;
+
+
+    private static class Node {
+        private Node next;
+        private int value;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
+}
